905c8c58dcd150b8f91d1673a749b0cffa528589
Artur Shaik <ashaihullin@gmail.com>
2015-07-30 23:06:44 +0600

    Fetchig configurations from git without clone it.

--- installer.sh
+++ installer.sh
@@ -4,8 +4,10 @@ cd "$(dirname "$0")"
 
 declare -a installList
 declare -a conflist
+declare -a remotelist
 command="null"
 repopath=$PWD
+githubRepo=https://github.com/artur-shaik/vimmer-dotfiles/
 
 function usage() {
     echo "./installer.sh [install [configuration [configuration [...]]]]"
@@ -30,6 +32,25 @@ function readArguments() {
     done
 }
 
+ignore=(.gitignore dests.txt installer.sh)
+
+function fetchRemoteList() {
+    echo "fetching list from github..."
+    for conf in `wget -qO- $githubRepo/tree/master | grep js-directory-link | sed 's/.*<a.*>\(.*\)<\/a>.*/\1/g'`; do
+        if [[ " ${ignore[@]} " =~ " ${conf} " ]]; then
+            continue
+        fi
+
+        if [[ " ${conflist[@]} " =~ " ${conf} " ]]; then
+            echo "local $conf"
+        else
+            echo "remote $conf"
+            conflist+=($conf)
+            remotelist+=($conf)
+        fi
+    done
+}
+
 function fillListLocal() {
     i=1
     for file in `ls -d */ | grep -v "backups" | cut -d/ -f 1`; do
@@ -47,8 +68,26 @@ function backup() {
     mv $1 $repopath/backups
 }
 
+function fetchRemoteDirectory() {
+    if ! hash svn 2> /dev/null; then
+        echo "you need 'svn' to fetch remote directories"
+        return 1
+    fi
+
+    echo "fetching $1 from remote repository..."
+    svn checkout $githubRepo/trunk/$1
+    return $?
+}
+
 function install() {
     for install in ${installList[*]}; do
+        if [[ " ${remotelist[@]} " =~ " ${install} " ]]; then
+            fetchRemoteDirectory $install
+            if [[ $? -eq 1 ]]; then
+                continue
+            fi
+        fi
+
         dest=$(eval echo `cat $repopath/dests.txt | grep "^$install*" | cut -d'=' -f 2`)
         if [[ $dest = '' ]]; then
             dest=~/
@@ -172,6 +211,7 @@ function main() {
     readArguments $@
 
     fillListLocal
+    fetchRemoteList
 
     while true; do
         if [[ $command = "null" ]]; then
