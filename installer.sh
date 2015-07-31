c55942c4e06d3b548220a6ed4880211b3630822c
Artur Shaik <ashaihullin@gmail.com>
2015-07-31 13:16:03 +0600

    Skip error messages when ls has no directories.

--- installer.sh
+++ installer.sh
@@ -79,7 +79,7 @@ function fetchRemoteList() {
 
 function fillListLocal() {
     i=1
-    for file in `ls -d */ | grep -v "backups" | cut -d/ -f 1`; do
+    for file in `ls -d */ 2> /dev/null | grep -v "backups" | cut -d/ -f 1`; do
         conflist[$i]=$file
         let i+=1
     done
