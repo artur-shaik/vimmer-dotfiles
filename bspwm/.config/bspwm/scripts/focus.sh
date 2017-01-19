bc97b83cde0f772497e8f530697ce51f4114cc6c
Artur Shaik <ashaihullin@gmail.com>
2017-01-19 14:18:32 +0600

    preserve focus parametes on select

--- bspwm/.config/bspwm/scripts/focus.sh
+++ bspwm/.config/bspwm/scripts/focus.sh
@@ -2,6 +2,8 @@
 
 dir=$1
 
+follows_monitor_current=$(bspc config pointer_follows_monitor)
+follows_focus_current=$(bspc config pointer_focus_monitor)
 bspc config pointer_follows_monitor true
 bspc config pointer_follows_focus true
 if ! bspc node -f $dir.local; then
@@ -9,5 +11,5 @@ if ! bspc node -f $dir.local; then
     bspc monitor -f $dir
     bspc config focus_follows_pointer true
 fi
-bspc config pointer_follows_monitor false
-bspc config pointer_follows_focus false
+bspc config pointer_follows_monitor $follows_monitor_current
+bspc config pointer_follows_focus $follows_focus_current
