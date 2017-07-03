bc5c5d6dd167fccfec27d50bd48ffa637e5f1529
Artur Shaik <ashaihullin@gmail.com>
2017-07-04 00:24:14 +0600

    use window_grid to move windows

--- bspwm/.config/bspwm/scripts/move.sh
+++ bspwm/.config/bspwm/scripts/move.sh
@@ -7,17 +7,19 @@ mon=$(bspc query -M -m)
 
 # are we floating? move a percentage of the monitor resolution.
 if bspc query -N -n $node.floating > /dev/null; then
-    percent=5
+    # percent=5
     case $dir in
-        west)  targetProp=width; sign=-;;
-        east)  targetProp=width;;
-        north) targetProp=height; sign=-;;
-        south) targetProp=height;;
+        west)  props="-x -1";;
+        east)  props="-x +1";;
+        north) props="-y -1";;
+        south) props="-y +1";;
     esac
 
-    moveArgs="$sign$(echo "$percent/100*$(bspc query -T -m | jq .rectangle.$targetProp)" | bc -l)"
-    [ $targetProp = "height"  ] && moveArgs="0 $moveArgs" || moveArgs="$moveArgs 0"
-    bspc node -v $moveArgs
+    window_grid -d 16 $props
+
+    # moveArgs="$sign$(echo "$percent/100*$(bspc query -T -m | jq .rectangle.$targetProp)" | bc -l)"
+    # [ $targetProp = "height"  ] && moveArgs="0 $moveArgs" || moveArgs="$moveArgs 0"
+    # bspc node -v $moveArgs
 else
     # we're tiled. TODO: re-evaluate this
     if ! bspc node -f $dir.local; then
